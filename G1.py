82c57d5ccd210c7363ae87d6d87c85b5cb0ced6d
symAndrew50 <117838737+symAndrew50@users.noreply.github.com>
2024-02-29 16:40:25 +0000

    hehe

--- G1.py
+++ G1.py
@@ -1,11 +1,45 @@
 # Connected components
 
 edges = [
-    [],
-    [],
-    [],
-    [],
-    [],
-    []
+    ['A', 'B'],
+    ['A', 'C'],
+    ['A', 'D'],
+    ['B', 'E'],
+    ['D', 'E'],
+    ['F', 'H'],
+    ['F', 'G'],
+    ['I', 'J']
 ]
+nodes = ['A','B','C','D','E','F','G','H','I','J','K']
+graph = {}
+
+
+def dfs(graph, node, visited):
+    
+    print(node)
+    visited.add(node)
+
+    for item in graph[node]:
+        if item not in visited:
+            dfs(graph,item,visited)
+
+
+for key in nodes:
+    graph[key] = []
+
+for (u,v) in edges:
+    graph[u].append(v)
+    graph[u].append(u)
+
+# print(graph)
+visited = set()
+answer = []
+for item in nodes:
+    if item not in visited:
+        temp = dfs(graph,'A', visited)
+        answer.append(temp)
+
+print(answer)
+
+
 
