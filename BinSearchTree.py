27ce1485306730ca1923babef5013eac1bb4547b
symAndrew50 <117838737+symAndrew50@users.noreply.github.com>
2024-03-04 13:56:11 +0000

    "ehhh"

---
+++ BinSearchTree.py
@@ -0,0 +1,86 @@
+class Tree:
+    def  __init__(self, data, left =None, right = None):
+        self.data = data
+        self.left = left
+        self.right = right
+    
+    def insert(self,data):
+        if (self.data == data):
+            return False
+        elif (self.data > data):
+            if self.left is not None:
+                return self.left.insert(data)
+            else:
+                self.left = Tree(data)
+                return True        
+        else:
+            if self.right is not None:
+                return self.right.insert(data)
+            else:
+                self.right = Tree(data)
+                return Tree
+    
+    def find(self,data):
+        if self.data == data:
+            return data
+
+        elif self.data > data:
+            if self.left is None:
+                return False
+            else:
+                return self.left.find(data)
+        
+        elif self.data < data:
+            if self.right is None:
+                return False
+            else:
+                return self.right.find(data)    
+
+    def get_size(self):
+        if self.left is not None and self.right is not None:
+            return 1+ self.left.get_size() + self.right.get_size()
+        elif self.left:
+            return 1+self.left.get_size()
+        elif self.right:
+            return 1
+
+    def preorder(self):
+        if self is not None:
+            print(self.data, end= ' ')
+
+            if self.left is not None:
+                self.left.preorder()
+            if self.right:
+                self.right.preorder()
+
+    def inorder(self):
+        if self is not None:
+            if self.left is not None:
+                self.left.inorder()
+            print(self.data, end= ' ')
+
+            if self.right is not None:
+                self.right.inorder()
+
+
+if __name__ == "__main__":
+    tree = Tree(7)
+    tree.insert(9)
+
+    nodes = [15,10,2,12,3,1,13,6,11,4,14,9]
+
+    for i in nodes:
+        tree.insert(i)
+    
+    for i in range(16):
+        print(tree.find(i), end=' ')
+    
+    print("\n", tree.get_size())
+
+
+    tree.preorder()
+    print()
+
+    tree.inorder()
+    print()
+
